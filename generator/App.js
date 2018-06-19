8a0807f8e5f9f7641a3167b75b5dfed359700a1c
Mike Grabowski <grabbou@gmail.com>
2018-06-19 10:56:03 +0200

    Add react-native init template

---
+++ generator/App.js
@@ -0,0 +1,9 @@
+/**
+ * Do not modify this file - it is a proxy to your `App.re` file
+ * located in the `src/` folder.
+ *
+ * `lib/js` is automatically generated first time you run
+ * `yarn build` or `yarn watch` and contains compiled Reason
+ * code.
+ */
+export { app as default } from "./lib/js/src/App.js";
